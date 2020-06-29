79178febad42ca2b04826e5bd9779a55b00af11c
ravi19999 <dahalravindra@gmail.com>
2020-06-29 15:33:53 +0545

    :bulb: added comments to the stepThroughCell function

--- index.js
+++ index.js
@@ -5,53 +5,76 @@ const {
   World,
   Bodies,
   MouseConstraint,
-  Mouse
+  Mouse,
 } = Matter;
 
+const cells = 3;
 const width = 800;
 const height = 600;
 
 const engine = Engine.create();
-const {
-  world
-} = engine;
+const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
     wireframes: false,
     width,
-    height
+    height,
   },
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}));
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 const walls = [
   Bodies.rectangle(400, 0, 800, 40, {
-    isStatic: true
+    isStatic: true,
   }),
   Bodies.rectangle(400, 600, 800, 40, {
-    isStatic: true
+    isStatic: true,
   }),
   Bodies.rectangle(0, 300, 40, 600, {
-    isStatic: true
+    isStatic: true,
   }),
   Bodies.rectangle(800, 300, 40, 600, {
-    isStatic: true
-  })
+    isStatic: true,
+  }),
 ];
 World.add(world, walls);
 
 //Maze generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
 
-const verticals = Array(3).fill(null).map(() => Array(2).fill(false))
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
 
-const horizontals = Array(2).fill(null).map(() => Array(3).fill(false));
-console.log(horizontals);
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the cell at [row,column], then return.
+  //Mark this cell as being visited.
+  //Assemble randomly-ordered list of neighbours.
+  //For each neighbour...
+  //See if that neighbour is out of bounds.
+  //If we have visited that neighbour, continue to next neighbour.
+  //Remove a wall from either the verticals array or horizontals array.
+  //Visit that next cell.
+};
+
+stepThroughCell(startRow, startColumn);
